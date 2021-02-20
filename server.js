c08175654f044b32ffe83c9f242a427321c60a27
Dan20402 <dan20402@gmail.com>
2021-02-20 16:01:39 -0300

    Add routes and controllers

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const bootcamps = require('./routes/bootcamps');
 //load env vars
 dotenv.config({ path: `./config/config.env` });
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from express');
-});
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT;
 
