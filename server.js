44658066c4da6da0aa8205d1109da0e4b418f070
Dan20402 <dan20402@gmail.com>
2021-02-20 23:29:21 -0300

    routes and controller bug fixed

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps');
-//load env vars
+const morgan = require('morgan');
+
 dotenv.config({ path: `./config/config.env` });
 
 const app = express();
 
+//Logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use('/api/v1/bootcamps', bootcamps);
 
-const PORT = process.env.PORT;
-
 app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  process.env.PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  )
 );
