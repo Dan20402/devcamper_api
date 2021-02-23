f40648c662fca39b4047264e240c531c064ca228
Dan20402 <dan20402@gmail.com>
2021-02-22 22:37:24 -0300

    Finish get getall and post bootcamps

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const bootcamps = require('./routes/bootcamps');
+
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: `./config/config.env` });
 
+connectDB();
+
 const app = express();
+
+app.use(express.json());
+
+const bootcamps = require('./routes/bootcamps');
 
 //Logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -14,9 +22,17 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(
+const server = app.listen(
   process.env.PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+      .yellow.bold
   )
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
